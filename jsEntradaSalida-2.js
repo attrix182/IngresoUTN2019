7f150fa10a9951d205417e53f2998e3007bdc1e6
attrix182 <44885834+attrix182@users.noreply.github.com>
2019-02-06 12:23:41 -0300

    Add files via upload

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,10 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+
+function Mostrar()
+{
+	nombre = prompt("Escriba su nombre")
+
+    alert(nombre)
+}
+
