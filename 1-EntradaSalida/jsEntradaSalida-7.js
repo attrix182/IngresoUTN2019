41a6cda14e2c83c879bf8941baa188da6fd8985a
Luciano Sinisterra <44885834+attrix182@users.noreply.github.com>
2019-02-06 17:18:45 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -2,23 +2,40 @@
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
+
 function sumar()
 {	
-	
+    num1 = document.getElementById("numeroUno").value;
+    num2 = document.getElementById("numeroDos").value;
+    sum = parseInt(num1) + parseInt(num2);
+    alert("La suma es " + sum)
 }
 
 function restar()
 {
-	
+    num1 = document.getElementById("numeroUno").value;
+    num2 = document.getElementById("numeroDos").value;
+    res = parseInt(num1) - parseInt(num2);
+
+	alert("La resta es " + res)
 }
 
 function multiplicar()
 { 
-	
+    num1 = document.getElementById("numeroUno").value;
+    num2 = document.getElementById("numeroDos").value;
+    mult = parseInt(num1) * parseInt(num2);
+
+	alert("La multiplicacion es " + mult)
 }
 
 function dividir()
 {
-	
+    num1 = document.getElementById("numeroUno").value;
+    num2 = document.getElementById("numeroDos").value;
+    div = parseInt(num1) / parseInt(num2);
+
+	alert("La division es " + div)
 }
+
 
