afd4c4b5ef4722b6b4c714586bb85d5f8f8b4df5
Luciano Sinisterra <44885834+attrix182@users.noreply.github.com>
2019-02-20 22:25:10 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,34 @@
 function Mostrar()
 {
 //tomo el mes 
-var mesDelAño = document.getElementById('mes').value;
+var mesDelAnio = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+switch(mesDelAnio) 
+{
+    case "Febrero":
+    {
+        alert("Este mes tiene 28 dias");
+        break;
+    }
+    case "Enero":
+    case "Marzo":
+    case "Mayo":
+    case "Julio":
+    case "Junio":
+    case "Octubre":
+    case "Diciembre":
+    {
+        alert("Este mes tiene 31 dias" );
+        break;
+    }
+    default:
+    {
+        alert( "Este mes tiene 30 dias");
+        break;
+    }
+}
 	
-	
 
 
-
-}//FIN DE LA FUNCIÓN
+}
+//FIN DE LA FUNCIÓN
